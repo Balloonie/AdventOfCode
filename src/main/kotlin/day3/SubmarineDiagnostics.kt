e63a9a44d41b0e847c753c7aee14bd4bc4f09286
balloonie <sebastian2004@gmx.de>
2021-12-11 15:38:54 +0100

    day 3 part 2

--- src/main/kotlin/day3/SubmarineDiagnostics.kt
+++ src/main/kotlin/day3/SubmarineDiagnostics.kt
@@ -5,8 +5,10 @@ import java.io.File
 
 
 fun main(args: Array<String>) {
+
     val diagnostics = loadResource("day3/Diagnostics")
     println(calculatePowerConsumption(diagnostics))
+    println(calculateLifeSupport(diagnostics))
 }
 
 fun calculatePowerConsumption(diagnostics: File): Int {
@@ -32,4 +34,36 @@ fun calculatePowerConsumption(diagnostics: File): Int {
     val epsilon = epsilonBits.joinToString(separator = "") { it.toString() }.toInt(2)
 
     return gamma * epsilon
+}
+
+fun calculateLifeSupport(diagnostics: File): Int {
+
+    val diagnosticLines = diagnostics.readLines()
+    val oxygen = findLifeSupportParameterValue(diagnosticLines, LifeSupportParameter.OXYGEN)
+    val carbonDioxide = findLifeSupportParameterValue(diagnosticLines, LifeSupportParameter.CARBON_DIOXIDE)
+
+    return oxygen * carbonDioxide
+}
+
+fun findLifeSupportParameterValue(diagnosticLines: List<String>, parameter: LifeSupportParameter): Int {
+    var prefix = ""
+    var lifeSupportParameterValue = -1
+    while (lifeSupportParameterValue < 0) {
+        val ones = diagnosticLines.count { it.startsWith(prefix + "1") }
+        val zeroes = diagnosticLines.count { it.startsWith(prefix + "0") }
+        prefix += parameter.evaluateBits(ones, zeroes)
+        diagnosticLines.singleOrNull { it.startsWith(prefix) }?.let { lifeSupportParameterValue = it.toInt(2) }
+    }
+    return lifeSupportParameterValue
+}
+
+enum class LifeSupportParameter {
+    OXYGEN {
+        override fun evaluateBits(oneCount: Int, zeroCount: Int): String =  if (oneCount >= zeroCount) "1" else "0"
+    },
+    CARBON_DIOXIDE {
+        override fun evaluateBits(oneCount: Int, zeroCount: Int): String = if (oneCount >= zeroCount) "0" else "1"
+    };
+
+    abstract fun evaluateBits(oneCount: Int, zeroCount: Int): String
 }
