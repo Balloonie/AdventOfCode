f1c5f1ec5eef10cb76552aab6919af2696c047dd
balloonie <sebastian2004@gmx.de>
2021-12-12 20:45:51 +0100

    day 7 part 1

---
+++ src/main/kotlin/day7/CrabAlignment.kt
@@ -0,0 +1,36 @@
+package day7
+
+import loadResource
+import java.io.File
+import kotlin.math.abs
+
+
+fun main(args: Array<String>) {
+    val startPositions = createStartingPositions(loadResource("day7/CrabAlignment"))
+    println(calculateOptimalAlignmentFuelCost(startPositions))
+
+}
+
+fun calculateOptimalAlignmentFuelCost(startPositions: List<Int>): Int {
+    var max = 0
+    var min = Int.MAX_VALUE
+
+    for (startPosition in startPositions) {
+        if (startPosition > max) max = startPosition
+        if (startPosition < min) min = startPosition
+    }
+
+    var fuel = Int.MAX_VALUE
+
+    for (targetPosition in min..max) {
+        val fuelCost = calculateFuelCost(startPositions, targetPosition)
+        if (fuelCost < fuel) fuel = fuelCost
+        println("Target: $targetPosition, Fuelcost: $fuelCost")
+    }
+
+    return fuel
+}
+
+fun calculateFuelCost(startPositions: List<Int>, targetPosition: Int): Int = startPositions.map { abs(targetPosition - it) }.sum()
+
+fun createStartingPositions(data: File): List<Int> = data.readLines().flatMap { it.split(",").map { p -> p.toInt() } }
