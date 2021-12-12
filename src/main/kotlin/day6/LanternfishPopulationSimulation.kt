20ab28eaebbeaccc8bf8c717180516b1e3d9c19b
balloonie <sebastian2004@gmx.de>
2021-12-12 19:45:59 +0100

    day 6 part 2 refactoring

--- src/main/kotlin/day6/LanternfishPopulationSimulation.kt
+++ src/main/kotlin/day6/LanternfishPopulationSimulation.kt
@@ -2,28 +2,36 @@ package day6
 
 import loadResource
 import java.io.File
+import kotlin.math.round
 
 fun main(args: Array<String>) {
     val initialPopulation = createPopulation(loadResource("day6/LanternfishesState"))
-    val population = simulatePopulation(initialPopulation, 80)
-    println(population.size)
+    println(initialPopulation.size)
+    val population = simulatePopulation(initialPopulation, 256)
+    println(population)
 }
 
-fun simulatePopulation(initialPopulation: List<Int>, days: Int): List<Int> {
-    val population = initialPopulation.toMutableList()
-    var spawn = 0
-    for (i in 1..days) {
-        for ((index, pop) in  population.withIndex()) {
-            if (pop == 0) spawn++
-            population[index] = if (pop == 0) 6 else pop - 1
-        }
-        for (i in 1..spawn) {
-            population.add(8)
-        }
-        spawn = 0
+fun simulatePopulation(initialPopulation: List<Int>, days: Int): Long {
+    val generation = (0..8).associateWith { 0L }.toMutableMap()
+
+    for (pop in initialPopulation) {
+        generation[pop] = generation[pop]!! + 1L
     }
-    return population
+
+    for (day in 1..days) {
+        val cycleFinished = generation[0]!!
+        generation[0] = generation[1]!!
+        generation[1] = generation[2]!!
+        generation[2] = generation[3]!!
+        generation[3] = generation[4]!!
+        generation[4] = generation[5]!!
+        generation[5] = generation[6]!!
+        generation[6] = generation[7]!! + cycleFinished
+        generation[7] = generation[8]!!
+        generation[8] = cycleFinished
+    }
+
+    return generation.values.sum()
 }
 
 fun createPopulation(data: File): List<Int> = data.readLines().flatMap { it.split(",").map { p -> p.toInt() } }
-
